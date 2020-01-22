4f7ad93bbed5822884d3448b4f7eb683b5487dac
MinexAutomation <minex.automation@gmail.com>
2020-01-21 16:38:02 -0800

    Added default DataDirectoryNameConvention implementation for IDataDirectoryNameConvention service.

---
+++ source/R5T.Dalkeith.Default/Code/Services/Implementations/DataDirectoryNameConvention.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace R5T.Dalkeith.Default
+{
+    public class DataDirectoryNameConvention : IDataDirectoryNameConvention
+    {
+        public const string DataDirectoryName = "Data";
+
+
+        public string GetDataDirectoryName()
+        {
+            return DataDirectoryNameConvention.DataDirectoryName;
+        }
+    }
+}
