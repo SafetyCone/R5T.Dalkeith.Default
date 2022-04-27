f78810df2cbe0580c0e6837fd5a82cab12c007e8
davidcoats <david@getsafetycone.com>
2022-04-27 15:29:09 -0700

    Interim changes.

--- source/R5T.Dalkeith.Default/Code/Services/Implementations/DataDirectoryNameConvention.cs
+++ source/R5T.Dalkeith.Default/Code/Services/Implementations/DataDirectoryNameConvention.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;using R5T.T0064;
 
 
 namespace R5T.Dalkeith.Default
-{
-    public class DataDirectoryNameConvention : IDataDirectoryNameConvention
+{[ServiceImplementationMarker]
+    public class DataDirectoryNameConvention : IDataDirectoryNameConvention,IServiceImplementation
     {
         public const string DataDirectoryName = "Data";
 
